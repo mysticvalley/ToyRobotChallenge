cb75a231b36ced0110b2981d916e3308267de8f2
Rajan Maharjan <rmjn.mdev@gmail.com>
2020-02-22 20:42:20 +1100

    Adds xcode project in the repository

---
+++ ToyRobotChallengeTests/ToyRobotChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  ToyRobotChallengeTests.swift
+//  ToyRobotChallengeTests
+//
+//  Created by Rajan Maharjan on 22/2/20.
+//  Copyright © 2020 Rajan Maharjan. All rights reserved.
+//
+
+import XCTest
+@testable import ToyRobotChallenge
+
+class ToyRobotChallengeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
