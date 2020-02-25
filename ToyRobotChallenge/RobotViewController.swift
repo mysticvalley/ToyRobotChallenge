9705ad8dd2b68f538c52255ef76f27707507dbdb
Rajan Maharjan <rmjn.mdev@gmail.com>
2020-02-25 11:52:38 +1100

    Formats source file using swiftformat

--- ToyRobotChallenge/RobotViewController.swift
+++ ToyRobotChallenge/RobotViewController.swift
@@ -1,8 +1,8 @@
 import UIKit
 
 final class RobotViewController: UIViewController {
+    // MARK: - Outlets
 
-    // MARK:- Outlets
     @IBOutlet private var textFieldXValue: UITextField!
     @IBOutlet private var textFieldYValue: UITextField!
     @IBOutlet private var segmentControlDirection: UISegmentedControl!
@@ -12,17 +12,20 @@ final class RobotViewController: UIViewController {
     @IBOutlet private var buttonMove: UIButton!
     @IBOutlet private var buttonReport: UIButton!
     @IBOutlet private var buttonReset: UIButton!
-    
-    // MARK:- Private Variables
+
+    // MARK: - Private Variables
+
+    private let board = Board(with: 5)
     private lazy var robot = Robot()
     private var commandText = ""
-    
-    // MARK:- View Controller life cycle
+
+    // MARK: - View Controller life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
-    
+
     // Setup view and basic routine.
     private func setupView() {
         textFieldXValue.becomeFirstResponder()
@@ -30,9 +33,9 @@ final class RobotViewController: UIViewController {
         textFieldYValue.delegate = self
         setupHideKeyboardOnTap()
     }
-    
-    // MARK:- Private Functions
-    
+
+    // MARK: - Private Functions
+
     // Reset robot's input fields such as X, Y, Direction and Command.
     private func resetRobot() {
         textFieldCommand.text = ""
@@ -41,68 +44,67 @@ final class RobotViewController: UIViewController {
         textFieldXValue.text = ""
         textFieldYValue.text = ""
     }
-    
+
     // shows alert to get the Robot location.
     private func showAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okButton = UIAlertAction(title: "Ok", style: .default, handler: { [weak self] action in
+        let okButton = UIAlertAction(title: "Ok", style: .default, handler: { [weak self] _ in
             self?.resetRobot()
         })
-        
+
         alert.addAction(okButton)
-        
-        self.present(alert, animated: true, completion: nil)
+
+        present(alert, animated: true, completion: nil)
     }
-    
-    // MARK:- Button actions
-    
+
+    // MARK: - Button actions
+
     // Done button action which shows Robot's current location for example (1 3 N)
-    @IBAction func reportButtonAction(_ sender: Any) {
-        
+    @IBAction func reportButtonAction(_: Any) {
         guard let xValue = textFieldXValue.text,
             let yValue = textFieldYValue.text,
             let rawValueDirection = Direction(rawValue: segmentControlDirection.selectedSegmentIndex + 1),
             let command = textFieldCommand?.text else { return }
-        
+
         robot.setPosition(xPoint: Int64(xValue) ?? 0,
                           yPoint: Int64(yValue) ?? 0,
-                          direction: rawValueDirection)
-        
+                          direction: rawValueDirection,
+                          board: board)
+
         robot.processRobotCommand(command: command)
-        
-        
+
         showAlert(title: "Output:",
                   message: robot.convertRobotPositionToString())
     }
-    
+
     // Left button action which performed move command for the Robot.
-    @IBAction func leftButtonAction(_ sender: Any) {
+    @IBAction func leftButtonAction(_: Any) {
         commandText.append("L")
         textFieldCommand.text = commandText
     }
-    
+
     // Right button action which performed move command for the Robot.
-    @IBAction func rightButtonAction(_ sender: Any) {
+    @IBAction func rightButtonAction(_: Any) {
         commandText.append("R")
         textFieldCommand.text = commandText
     }
-    
+
     // Move button action which performed move command for the Robot.
-    @IBAction func moveButtonAction(_ sender: Any) {
+    @IBAction func moveButtonAction(_: Any) {
         commandText.append("M")
         textFieldCommand.text = commandText
     }
-    
+
     // Reset button action which clears robot's input fields such as X, Y, Direction and Command.
-    @IBAction func resetButtonAction(_ sender: Any?) {
+    @IBAction func resetButtonAction(_: Any?) {
         resetRobot()
     }
 }
 
 extension RobotViewController: UITextFieldDelegate {
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange,
+    func textField(_ textField: UITextField, shouldChangeCharactersIn _: NSRange,
                    replacementString string: String) -> Bool {
-        let allowedCharSet = NSCharacterSet(charactersIn:Constants.RobotInputs.allowedRegx).inverted
+        let allowedCharSet = NSCharacterSet(charactersIn: board.allowedRegx).inverted
         let compSepByCharInSet = string.components(separatedBy: allowedCharSet)
         let numberFiltered = compSepByCharInSet.joined(separator: "")
         if string == numberFiltered {
@@ -117,12 +119,10 @@ extension UIViewController {
         view.addGestureRecognizer(endEditingRecognizer())
         navigationController?.navigationBar.addGestureRecognizer(endEditingRecognizer())
     }
-        
+
     private func endEditingRecognizer() -> UIGestureRecognizer {
         let tap = UITapGestureRecognizer(target: view, action: #selector(view.endEditing(_:)))
         tap.cancelsTouchesInView = false
         return tap
     }
 }
-
-
--- ToyRobotChallengeTests/ToyRobotChallengeTests.swift
+++ ToyRobotChallengeTests/ToyRobotChallengeTests.swift
@@ -1,8 +1,7 @@
-import XCTest
 @testable import ToyRobotChallenge
+import XCTest
 
 class ToyRobotChallengeTests: XCTestCase {
-    
     let robot = Robot()
 
     func testRobotFacingNorth() {
@@ -11,53 +10,48 @@ class ToyRobotChallengeTests: XCTestCase {
         robot.setPosition(xPoint: 0,
                           yPoint: 2,
                           direction: .north)
-        
+
         robot.processRobotCommand(command: "LMMLMLMMLMM")
         XCTAssertEqual(expectedOutput, robot.convertRobotPositionToString())
-        
     }
-    
+
     func testRobotFacingEast() {
         let expectedOutput = "4 0 EAST"
         robot.setPosition(xPoint: 2,
                           yPoint: 1,
                           direction: .east)
-        
-        
+
         robot.processRobotCommand(command: "MMRMMRMRRM")
-        
+
         let robotCurrentPosition = robot.currentPosition()
         print(robotCurrentPosition)
         XCTAssertEqual(expectedOutput, robot.convertRobotPositionToString())
-        
     }
-    
+
     func testRobotFacingWest() {
         let expectedOutput = "1 4 WEST"
         robot.setPosition(xPoint: 1,
                           yPoint: 4,
                           direction: .south)
-        
+
         robot.processRobotCommand(command: "LMLMLMLMR")
         let robotCurrentPosition = robot.currentPosition()
         print(robotCurrentPosition)
         XCTAssertEqual(expectedOutput, robot.convertRobotPositionToString())
-        
     }
-    
+
     func testRobotFacingSouth() {
         let expectedOutput = "5 5 SOUTH"
         robot.setPosition(xPoint: 5,
                           yPoint: 4,
                           direction: .east)
-        
+
         robot.processRobotCommand(command: "LMLMLMLMLMMMRMR")
         let robotCurrentPosition = robot.currentPosition()
         print(robotCurrentPosition)
         XCTAssertEqual(expectedOutput, robot.convertRobotPositionToString())
-        
     }
-    
+
     func testRobotAvoidFallingFromTable() {
         let expectedOutput = "4 1 EAST"
         robot.setPosition(xPoint: 0,
